bb152f4128e574ef0b2df3ac857012a145cb3784
otteryc <otteryc210@gmail.com>
2024-07-17 20:41:49 +0800

    Improve header readability

--- inc/lib.h
+++ inc/lib.h
@@ -10,19 +10,19 @@ hv_t *new_negate_hypervector(hv_t *hv);
 hv_t *clone_hypervector(hv_t *hv);
 
 hv_t *new_permute_hypervector(hv_t *hv, uint32_t shift);
-#define hvops(name) hv_t *new_##name##_hypervector(hv_t *, hv_t *)
+#define hvops(name) hv_t *new_##name##_hypervector(hv_t *src1, hv_t *src2)
 hvops(and);
 hvops(or);
 hvops(xor);
 hvops(bind);
 hvops(bundle);
 #undef hvops
-#define hvops(name) void name##_hypervector(hv_t *, hv_t *, hv_t *)
+#define hvops(name) void name##_hypervector(hv_t *dest, hv_t *src1, hv_t *src2)
 hvops(and);
 hvops(or);
 hvops(xor);
 #undef hvops
-hv_t *new_bundle_multi_hypervector(struct list_head *);
+hv_t *new_bundle_multi_hypervector(struct list_head *head);
 hv_t *new_bind_multi_hypervector(struct list_head *head);
 
 void free_hypervector(hv_t *hv);
@@ -32,7 +32,7 @@ uint32_t dot_distance(hv_t *hv1, hv_t *hv2);
 double cosine_similarity(hv_t *hv1, hv_t *hv2);
 
 void chain_hypervector(struct list_head *head, hv_t *hv1, ...);
-void print_hypervector(hv_t *);
+void print_hypervector(hv_t *hv);
 struct list_head *query_memory(struct list_head *, hv_t *,
                                enum similarity_method);
 
